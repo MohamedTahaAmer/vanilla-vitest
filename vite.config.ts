fa88123af5092cb38b889c5d1281b90c3d8d21f9
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-02-13 18:48:38 +0200

    fix: :bug: nothing to commit, working tree clean, change the local prettier config to get sth to commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	// not include the testing code when running 'npm run build'
 	define: {
-		"import.meta.vitest": "undefined",
+		'import.meta.vitest': 'undefined',
 	},
 	test: {
 		// test along Code
-		includeSource: ["src/**/*.ts"],
+		includeSource: ['src/**/*.ts'],
 
 		// coverage output format
 		coverage: {
-			reporter: ["text", "json", "html"],
+			reporter: ['text', 'json', 'html'],
 		},
 	},
 })
